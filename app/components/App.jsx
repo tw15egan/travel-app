c36f29d5293602b3a6fbc23aa7a5c23f56132970
TJ Egan <twegan@us.ibm.com>
2016-07-05 22:00:48 -0500

    fix image links

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -20,14 +20,14 @@ export default class App extends React.Component {
           id: uuid.v4(),
           city: 'Paris',
           country: 'France',
-          image: 'http://i.imgur.com/07Nk5kz',
+          image: 'http://i.imgur.com/07Nk5kz.jpg',
           visited: true
         },
         {
           id: uuid.v4(),
           city: 'Kuala Lumpur',
           country: 'Malaysia',
-          image: 'http://i.imgur.com/qt6PR2f',
+          image: 'http://i.imgur.com/qt6PR2f.jpg',
           visited: false
         }
       ]
